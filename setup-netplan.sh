b2fd5da76d359c8b96be1097b2d0dd195d86b6fa
Pavel <32277697+rike18@users.noreply.github.com>
2025-11-28 23:40:10 +0300

    Update

--- setup-netplan.sh
+++ setup-netplan.sh
@@ -4,6 +4,53 @@ import ipaddress
 import argparse
 import yaml
 import os
+import subprocess
+import time
+
+def bring_iface_up(iface: str):
+    try:
+        subprocess.run(
+            ["ip", "link", "set", iface, "up"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            check=False
+        )
+    except Exception:
+        pass
+
+
+def get_iface_speed(iface: str) -> int | None:
+    speed_path = f"/sys/class/net/{iface}/speed"
+    if not os.path.isfile(speed_path):
+        return None
+
+    try:
+        with open(speed_path, "r") as f:
+            speed = f.read().strip()
+        return int(speed)
+    except Exception:
+        return None
+
+
+def detect_interfaces(target_speed: int, limit: int = None) -> list[str]:
+    found = []
+
+    for net in os.listdir("/sys/class/net"):
+        if net == "lo":
+            continue
+
+        bring_iface_up(net)
+        time.sleep(1)
+
+        speed = get_iface_speed(net)
+        if speed == target_speed:
+            if net not in found:
+                found.append(net)
+
+        if limit and len(found) >= limit:
+            break
+
+    return found
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip-address", required=True, default=None)
@@ -144,17 +191,27 @@ for i in network_list:
         ip_gateway = i["gateway"]
         vlan = int(i["vlan"])
 
+
+iface = detect_interfaces(10000, limit=2)
+
+if len(iface) == 2:
+    interfaces = iface
+else:
+    interfaces = detect_interfaces(1000)
+
+print("Detected interfaces:", interfaces)
+
 config_json = {
     "network": {
         "version": 2,
         "renderer": "networkd",
         "ethernets": {
-            "eth0": {},
-            "eth1": {}
+            interfaces[0]: {},
+            interfaces[1]: {}
         },
         "bonds": {
             "bond0": {
-                "interfaces": ["eth0", "eth1"],
+                "interfaces": interfaces,
                 "parameters": {
                     "mode": "802.3ad",
                     "mii-monitor-interval": 1,
